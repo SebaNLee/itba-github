0a167d8a2ea1382d65832a522c4d8e2ec7cc6c79
Sebalee_ZephyrusM16 <slee@itba.edu.ar>
2024-10-03 19:06:17 -0300

    feat: poo tp06 ej6 incompleto

---
+++ 20242Q/poo/laboratorio/tp06/ej6/src/PersonCollection.java
@@ -0,0 +1,12 @@
+
+import java.util.List;
+
+public interface PersonCollection {
+
+    void addPerson(Person aPerson);
+
+    List<Person> findByLastName(String lastName);
+
+    Person findByName(String firstName, String lastName);
+
+}
---
+++ 20242Q/poo/laboratorio/tp06/ej6/src/PersonCollectionImpl.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PersonCollectionImpl implements PersonCollection {
+
+
+    public void addPerson(Person aPerson) {
+
+
+    }
+
+    public List<Person> findByLastName(String lastName) {
+
+
+    }
+
+    public Person findByName(String firstName, String lastName) {
+
+
+    }
+
+    Set<String> e = new Set<>();
+
+
+}
+
+
+// 
