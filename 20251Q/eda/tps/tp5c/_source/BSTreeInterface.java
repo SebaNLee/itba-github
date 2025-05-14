a2fdbe8cbc779399778ea9bdc60116e61ef10dcf
Sebalee_ZephyrusM16 <slee@itba.edu.ar>
2025-05-14 10:36:44 -0300

    feat: eda tp5c source download

---
+++ 20251Q/eda/tps/tp5c/_source/BSTreeInterface.java
@@ -0,0 +1,18 @@
+package core;
+
+
+public interface BSTreeInterface<T extends Comparable<? super T>> {
+
+	void insert(T myData);
+
+	void preOrder();
+
+	void postOrder();
+
+	void inOrder();
+
+	NodeTreeInterface<T> getRoot();
+	
+	int getHeight();
+
+}
