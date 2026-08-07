e08ff5f021773c7c2f43a863d0f6ebac579ee188
SebaNLee <seba.nico.lee@gmail.com>
2026-08-07 00:14:53 -0300

    feat: cgraf tarea clase1

--- 20262Q/cgraf/tareas/clase1/templateBasico/src/sceneManager.js
+++ 20262Q/cgraf/tareas/clase1/templateBasico/src/sceneManager.js
@@ -22,17 +22,42 @@ export class SceneManager {
 			new THREE.TorusGeometry(1, 0.4, 16, 100),
 			new THREE.MeshPhongMaterial({ color: 0xff00ff })
 		);
-		torus.position.set(0, 1, 0);
+		torus.position.set(0, -3, 0);
 		scene.add(torus);
 
 		cone = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 16, 1), new THREE.MeshPhongMaterial({ color: 0x00ffff }));
-		cone.position.set(3, 1, 0);
+		cone.position.set(3, -3, 0);
 		scene.add(cone);
 
 		cube = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshPhongMaterial({ color: 0xffff00 }));
 		cube.rotation.set(0, Math.PI / 4, 0);
-		cube.position.set(-3, 1, 0);
+		cube.position.set(-3, -3, 0);
 		scene.add(cube);
+
+		// base grass
+		let grass = new THREE.Mesh(new THREE.BoxGeometry(10, 1, 10), new THREE.MeshPhongMaterial({ color: 0x00ff00 }));
+		grass.position.set(0, -0.5, 0);
+		scene.add(grass);
+
+		// castle base
+		let castleBase = new THREE.Mesh(new THREE.BoxGeometry(2, 1, 2), new THREE.MeshPhongMaterial({ color: 0xee9955 }));
+		castleBase.position.set(0, 0, -2);
+		scene.add(castleBase);
+
+		// caste tower
+		let castleTower = new THREE.Mesh(new THREE.CylinderGeometry(0.3, 0.3, 1), new THREE.MeshPhongMaterial({ color: 0xee9955 }));
+		castleTower.position.set(-1, 0 + 1 / 2, -1);
+		scene.add(castleTower);
+
+		// caste tower cap
+		let castleTowerCap = new THREE.Mesh(new THREE.ConeGeometry(0.4, 0.5), new THREE.MeshPhongMaterial({ color: 0xee9955 }));
+		castleTowerCap.position.set(-1, 1 + 0.5 / 2, -1);
+		scene.add(castleTowerCap);
+
+		// pond
+		let pond = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 0.1), new THREE.MeshPhongMaterial({ color: 0x3355aa }));
+		pond.position.set(0, 0, 2);
+		scene.add(pond)
 	}
 
 	animate() {
