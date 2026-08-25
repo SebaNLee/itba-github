72d817cb128a48c75c5c5155398fada0e35c5b29
SebaNLee <seba.nico.lee@gmail.com>
2026-08-25 15:00:58 -0300

    feat: bd2 tp4 ej1

---
+++ 20262Q/bd2/tps/tp4/ej1.sql
@@ -0,0 +1,43 @@
+
+-- a)
+DROP VIEW IF EXISTS envios500;
+
+CREATE VIEW envios500 AS
+SELECT id_proveedor, id_articulo, cantidad
+FROM envio
+WHERE cantidad >= 500;
+
+SELECT * FROM envios500;
+-- sí es modificable pues: conserva columnas PK, sin funciones de agregación ni DISTINCT ni subconsultas SELECT 
+
+-- b)
+DROP VIEW IF EXISTS envios500_999;
+
+CREATE VIEW envios500_999 AS
+SELECT *
+FROM envios500
+WHERE cantidad >= 500 AND cantidad <= 999; -- medio al pedo el >= 500
+
+SELECT * FROM envios500_999;
+-- sí es modificable: ídem y se crea a partir de VIEW modificable
+
+-- c)
+DROP VIEW IF EXISTS detalle_envios;
+
+CREATE VIEW detalle_envios AS
+SELECT descrip, peso, nombre, cantidad
+FROM envios500 e
+JOIN proveedor p ON p.id_proveedor = e.id_proveedor
+JOIN articulo a ON a.id_articulo = e.id_articulo; 
+
+SELECT * FROM detalle_envios;
+-- no es actualizable, no tiene columnas PK
+
+
+-- i) - iv)
+
+-- Básicamente no deja modificar con WCO si viola condición del VIEW, sino deja y desaparece del VIEW
+INSERT INTO ENVIOS500 VALUES ('PI', 'Al', 500);
+INSERT INTO ENVIOS500 VALUES ('P2', 'A2', 300);
+UPDATE ENVIOS500 SET cantidad=1000 WHERE id proveedor='Pl';
+UPDATE ENVIOS500 SET cantidad=100 WHERE id proveedor='Pl';
