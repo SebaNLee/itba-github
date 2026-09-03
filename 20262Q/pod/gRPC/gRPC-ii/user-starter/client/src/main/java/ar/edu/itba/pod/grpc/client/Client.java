3e673a46e10457b7a8265a6c214e080a8fbcc208
SebaNLee <seba.nico.lee@gmail.com>
2026-09-02 21:02:26 -0300

    feat: pod grpc-ii clase ej3

--- 20262Q/pod/gRPC/gRPC-ii/user-starter/client/src/main/java/ar/edu/itba/pod/grpc/client/Client.java
+++ 20262Q/pod/gRPC/gRPC-ii/user-starter/client/src/main/java/ar/edu/itba/pod/grpc/client/Client.java
@@ -6,7 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.grpc.client.ImportGrpcClients;
 
+import ar.edu.itba.pod.grpc.user.LoginInformation;
+import ar.edu.itba.pod.grpc.user.UserServiceGrpc;
+
+@ImportGrpcClients(target = "local", types = UserServiceGrpc.UserServiceBlockingStub.class)
 @SpringBootApplication
 public class Client {
 
@@ -17,9 +22,15 @@ public class Client {
     }
 
     @Bean
-    CommandLineRunner run() {
+    CommandLineRunner run(UserServiceGrpc.UserServiceBlockingStub stub) {
         return _ -> {
-            // TODO
+            var request = LoginInformation.newBuilder()
+                    .setUserName("foo")
+                    .setPassword("foopass")
+                    .build();
+            IO.println(stub.doLogin(request));
+
+            // TOOD uso de getRoles()...
         };
     }
 
--- 20262Q/pod/gRPC/gRPC-ii/user-starter/server/src/main/java/ar/edu/itba/pod/grpc/server/Servant.java
+++ 20262Q/pod/gRPC/gRPC-ii/user-starter/server/src/main/java/ar/edu/itba/pod/grpc/server/Servant.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.pod.grpc.server;
 
 import ar.edu.itba.pod.grpc.user.*;
+import io.grpc.stub.StreamObserver;
+
 import org.springframework.stereotype.Service;
 
 import module java.base;
@@ -33,4 +35,27 @@ public class Servant extends UserServiceGrpc.UserServiceImplBase {
 			"bar", UserRoles.newBuilder()
 					.putRolesBySite("abc.com", Role.SELLER).build());
 
+
+	@Override
+	public void doLogin(LoginInformation loginInformation, StreamObserver<User> responseObserver) {
+		String username = loginInformation.getUserName();
+		String password = loginInformation.getPassword();
+
+		if (passwordsByUser.containsKey(username) && passwordsByUser.get(username).equals(password)) {
+			responseObserver.onNext(users.get(username));
+		}
+		
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void getRoles(User user, StreamObserver<UserRoles> responseObserver) {
+		String username = user.getUserName();
+
+		if (userRolesMap.containsKey(username)) {
+			responseObserver.onNext(userRolesMap.get(username));
+		}
+
+		responseObserver.onCompleted();
+	}
 }
