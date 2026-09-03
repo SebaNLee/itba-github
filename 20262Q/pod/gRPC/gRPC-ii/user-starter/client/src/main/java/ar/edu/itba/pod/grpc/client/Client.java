01136ff7d5751c6e416a3c71daabd3f759dcdc88
SebaNLee <seba.nico.lee@gmail.com>
2026-09-02 21:26:47 -0300

    feat: pod grpc-ii clase ej4

--- 20262Q/pod/gRPC/gRPC-ii/user-starter/client/src/main/java/ar/edu/itba/pod/grpc/client/Client.java
+++ 20262Q/pod/gRPC/gRPC-ii/user-starter/client/src/main/java/ar/edu/itba/pod/grpc/client/Client.java
@@ -1,14 +1,26 @@
 package ar.edu.itba.pod.grpc.client;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.grpc.client.GrpcChannelFactory;
 import org.springframework.grpc.client.ImportGrpcClients;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
 import ar.edu.itba.pod.grpc.user.LoginInformation;
+import ar.edu.itba.pod.grpc.user.User;
+import ar.edu.itba.pod.grpc.user.UserRoles;
 import ar.edu.itba.pod.grpc.user.UserServiceGrpc;
 
 @ImportGrpcClients(target = "local", types = UserServiceGrpc.UserServiceBlockingStub.class)
@@ -22,16 +34,45 @@ public class Client {
     }
 
     @Bean
-    CommandLineRunner run(UserServiceGrpc.UserServiceBlockingStub stub) {
+    UserServiceGrpc.UserServiceFutureStub userFutureStub(GrpcChannelFactory channels) {
+        return UserServiceGrpc.newFutureStub(channels.createChannel("local"));
+    }
+
+    @Bean
+    CommandLineRunner run(UserServiceGrpc.UserServiceBlockingStub stub, UserServiceGrpc.UserServiceFutureStub futureStub) {
         return _ -> {
             var request = LoginInformation.newBuilder()
                     .setUserName("foo")
                     .setPassword("foopass")
                     .build();
-            IO.println(stub.doLogin(request));
+            
+            User user = stub.doLogin(request);
+            IO.println(user);
 
-            // TOOD uso de getRoles()...
+            // ej4
+            // getRoles() con FutureStub
+            ListenableFuture<UserRoles> listenableFuture = futureStub.getRoles(user);
+
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+            
+            try {
+                    Futures.addCallback(listenableFuture, new FutureCallback<>() {
+                    @Override
+                    public void onSuccess(UserRoles userRoles) {
+                        IO.println(userRoles);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable t) {
+                        log.error(t.getMessage());
+                        countDownLatch.countDown();
+                    }
+                }, executorService);                
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            
         };
     }
-
 }
